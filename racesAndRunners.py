ecee3a9245a7ab68bd987819f416b16732270a98
al-rawahi <87913058+al-rawahi@users.noreply.github.com>
2022-12-06 11:51:18 +0000

    Update racesAndRunners.py

--- racesAndRunners.py
+++ racesAndRunners.py
@@ -6,9 +6,8 @@ def read_integer_between_numbers(prompt, mini, maximum):
                 return users_input
             else:
                 print(f"Numbers from {mini} to {maximum} only.")
-        except ValueError:
+        except EOFError:
             print("Sorry -number only please")
-            break
 
 
 def read_nonempty_string(prompt):
